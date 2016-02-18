5f688b2bbfbd572d7096b7a3a7e0402055d75d54
Chad Bacon <Chadsbacon@gmail.com>
2016-02-17 22:45:26 -0700

    Finished creating Javadocs for all DTOs, Enums, and UI Interfaces.

--- src/com/cs428/pandemic/frontEnd/dataTransferObjects/UI_Card.java
+++ src/com/cs428/pandemic/frontEnd/dataTransferObjects/UI_Card.java
@@ -2,15 +2,30 @@ package com.cs428.pandemic.frontEnd.dataTransferObjects;
 
 /**
  * Created by Chad Bacon on 2/16/2016.
- * TODO: javadocs
+ *
+ * This class holds all of the necessary card data for the UI. Note that this class is used for
+ * both Player cards and Infection cards. The UI determine which card it is based on context since
+ * splitting the two into different classes would result in them being virtually identical.
  */
 public class UI_Card {
+
+    /**
+     * The name of the city associated with the card.
+     */
     private String cardName;
 
+    /**
+     *
+     * @param cardName The name of the city to be associated with this card.
+     */
     public UI_Card(String cardName) {
         this.cardName = cardName;
     }
 
+    /**
+     *
+     * @return The name of the city associated with the card.
+     */
     public String getCardName() {
         return cardName;
     }
