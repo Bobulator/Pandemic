cc4b5b44d0ce7992a39c1375d5f45c0f55666e3f
Andrew <wally451@sbcglobal.net>
2016-03-17 20:37:05 -0600

    implmeneted navigation drawer menu and pinch and zoom; still need to finish implementing both, but they both work

--- src/com/cs428/pandemic/frontEnd/gamePlay/BoardFragment.java
+++ src/com/cs428/pandemic/frontEnd/gamePlay/BoardFragment.java
@@ -4,11 +4,17 @@ import android.app.Fragment;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.cs428.pandemic.R;
@@ -23,12 +29,27 @@ import com.cs428.pandemic.R;
  */
 public class BoardFragment extends Fragment implements View.OnTouchListener {
 
+	// 3 possible states
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
+    
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+    
+    // Remember some things for zooming
+    PointF start = new PointF();
+    PointF mid = new PointF();
+    double oldDist = 1.0;
+	
+    private static final String TAG = "Touch";
+    
     private ImageView mBoard;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setHasOptionsMenu(false);	// I don't want an options menu on this fragment
     }
 
     @Override
@@ -37,14 +58,19 @@ public class BoardFragment extends Fragment implements View.OnTouchListener {
         View v = inflater.inflate(R.layout.fragment_game_board, parent, false);
         mBoard = (ImageView) v.findViewById(R.id.board_image_view);
         mBoard.setOnTouchListener(this);
+        
+        WindowManager wm = (WindowManager) getActivity().getSystemService(getActivity().WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
 
         // Display the game board
         mBoard.setImageBitmap(
                 // Use a bitmap to scale the image resource down so as not to use too much memory
                 // The width and height were arbitrarily chosen as placeholders until we can find
                 // a more consistent way to determine what size we should draw the image
-                decodeSampledBitmapFromResource(getResources(), R.drawable.game_board_nocities,
-                        400, 400));
+//                decodeSampledBitmapFromResource(getResources(), R.drawable.game_board_nocities, size.x, size.y));
+        		decodeSampledBitmapFromResource(getResources(), R.drawable.game_board_nocities, 800, 400));
 
         return v;
     }
@@ -87,8 +113,73 @@ public class BoardFragment extends Fragment implements View.OnTouchListener {
     }
 
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        return false;
+	public boolean onTouch(View v, MotionEvent event) {
+		ImageView view = (ImageView) v;
+        // make the image scalable as a matrix
+        view.setScaleType(ImageView.ScaleType.MATRIX);
+        double scale;
+	        
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+	        case MotionEvent.ACTION_DOWN: //first finger down only
+	            savedMatrix.set(matrix);
+	            start.set(event.getX(), event.getY());
+	            Log.d(TAG, "mode=DRAG" );
+	            mode = DRAG;
+	            break;
+	        case MotionEvent.ACTION_UP: //first finger lifted
+	        	break;
+	        case MotionEvent.ACTION_POINTER_UP: //second finger lifted
+	            mode = NONE;
+	            Log.d(TAG, "mode=NONE" );
+	            break;
+	        case MotionEvent.ACTION_POINTER_DOWN: //second finger down
+	            oldDist = spacing(event); // calculates the distance between two points where user touched.
+	            Log.d(TAG, "oldDist=" + oldDist);
+	            // minimal distance between both the fingers
+	            if (oldDist > 5f) {
+	                savedMatrix.set(matrix);
+	                midPoint(mid, event); // sets the mid-point of the straight line between two points where user touched.
+	                mode = ZOOM;
+	                Log.d(TAG, "mode=ZOOM" );
+	            }
+	            break;
+	
+	        case MotionEvent.ACTION_MOVE:
+	            if (mode == DRAG)
+	            { //movement of first finger
+	                matrix.set(savedMatrix);
+	                if (view.getLeft() >= -392)
+	                {
+	                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
+	                }
+	            }
+	            else if (mode == ZOOM) { //pinch zooming
+	                double newDist = spacing(event);
+	                Log.d(TAG, "newDist=" + newDist);
+	                if (newDist > 5f) {
+	                    matrix.set(savedMatrix);
+	                    scale = newDist/oldDist; //thinking I need to play around with this value to limit it**
+	                    matrix.postScale((float)scale, (float)scale, mid.x, mid.y);
+	                }
+	            }
+	            break;
+	    }
+	    // Perform the transformation
+	    view.setImageMatrix(matrix);
+	
+	    return true;
+	}
+	
+	private double spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
     }
 
 }
--- src/com/cs428/pandemic/frontEnd/gamePlay/GamePlayActivity.java
+++ src/com/cs428/pandemic/frontEnd/gamePlay/GamePlayActivity.java
@@ -4,17 +4,21 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.view.WindowManager;
 
 import com.cs428.pandemic.R;
 
-public class GamePlayActivity extends Activity {
-
+public class GamePlayActivity extends Activity 
+		implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+	
+	private NavigationDrawerFragment mNavigationDrawerFragment;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		
 		if (Build.VERSION.SDK_INT < 16) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -22,18 +26,47 @@ public class GamePlayActivity extends Activity {
         	View decorView = getWindow().getDecorView();
         	int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         	decorView.setSystemUiVisibility(uiOptions);
-        	ActionBar actionBar = getActionBar();
-			if (actionBar != null) {
-				actionBar.hide();
-			}
         }
-
-
+		
 		setContentView(R.layout.activity_gameplay);
+		
+		mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getFragmentManager().findFragmentById(R.id.navigation_drawer);
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+		
+        if (getActionBar() != null)
+        	getActionBar().setDisplayHomeAsUpEnabled(false);
+		
+		
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.gameplay_container, new BoardFragment()).commit();
 		}
 	}
+	
+	@Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // TODO: handle menu item selection        
+    }
 
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+	public void restoreActionBar() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+    }	
 }
