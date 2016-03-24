4d8509f7d50c66c995464514af02501430a6bf72
Chad <chadsbacon@gmail.com>
2016-03-24 14:44:01 -0600

    Created a BoardDrawer class to handle drawing the elements of the board. Refactored the BoardFragment by moving methods involved with board drawing into the BoardDrawer class.

---
+++ src/com/cs428/pandemic/frontEnd/gamePlay/boardFragment/BoardDrawer.java
@@ -0,0 +1,99 @@
+package com.cs428.pandemic.frontEnd.gamePlay.boardFragment;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * This class will handle drawing all of the components of the board, including the board itself,
+ * cities, city connections, disease, built research stations, and pawn locations.
+ *
+ * Created by Chad on 3/24/2016.
+ */
+public class BoardDrawer {
+
+    public BoardDrawer() {}
+
+    public Bitmap createBitmap(Resources res, int resId, int reqWidth, int reqHeight) {
+        return decodeSampledBitmapFromResource(res, resId, reqWidth, reqHeight);
+    }
+
+    public Canvas drawBoard(Bitmap bitmap) {
+
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+
+        final int width = canvas.getWidth();
+        final int height = canvas.getHeight();
+
+        // Not sure how the pixel locations are related to the initial images. These are simply
+        // arbitrary proof-of-concept lines to demonstrate that this works.
+        Paint p = new Paint();
+        p.setColor(Color.WHITE);
+        canvas.drawLine(80, 80, 350, 350, p);
+        canvas.drawLine(80, 80, 1900, 1000, p);
+        canvas.drawLine(80, 80, 1900, 350, p);
+        canvas.drawLine(80, 80, 350, 1000, p);
+
+        return canvas;
+    }
+
+    /**
+     * Construct a Bitmap of the given resource that is as small as possible while remaining
+     * larger than the given width and height.
+     *
+     * @param res The resources from which the image is located.
+     * @param resId The specific id of the image to generate a Bitmap for.
+     * @param reqWidth The width constraint.
+     * @param reqHeight The height constraint.
+     * @return A Bitmap such that the width and height are as small as possible while remaining
+     * larger than the reqWidth and reqHeight.
+     */
+    private Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                   int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        options.inMutable = true;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+    /**
+     * Determine the extent to which the Bitmap dimensions should be reduced from the original image
+     * size using the given constraints. The reduction is determined such that the original image is
+     * reduced to be as small as possible while remaining larger than the given width and height.
+     *
+     * @param options The Bitmap options from which the original image dimensions are retrieved.
+     * @param reqWidth The width constraint.
+     * @param reqHeight The height constraint.
+     *
+     * @return The sample size by which the original image should be reduced.
+     */
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw width and height of the image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+        // width and height larger than the requested width and height.
+        while ((height / inSampleSize) > reqHeight
+                && (width / inSampleSize) > reqWidth) {
+            inSampleSize *= 2;
+        }
+        inSampleSize /= 2;
+
+        return inSampleSize;
+    }
+}
--- src/com/cs428/pandemic/frontEnd/gamePlay/boardFragment/BoardFragment.java
+++ src/com/cs428/pandemic/frontEnd/gamePlay/boardFragment/BoardFragment.java
@@ -5,10 +5,7 @@ import android.app.FragmentManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Matrix;
-import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.drawable.BitmapDrawable;
@@ -60,6 +57,7 @@ public class BoardFragment extends Fragment implements View.OnTouchListener {
     private ImageView boardImageView;
 
     private IModelInterface modelFacade;
+    private BoardDrawer boardDrawer;
 
     private List<UI_Player> players;
 
@@ -79,26 +77,16 @@ public class BoardFragment extends Fragment implements View.OnTouchListener {
         Point size = new Point();
         display.getSize(size);
 
+        // The boardDrawer class will do all of the necessary drawing to create the board.
+        boardDrawer = new BoardDrawer();
+
         // Use a bitmap to scale the image resource down so as not to use too much memory.
         // The width and height were arbitrarily chosen as placeholders until we can find
         // a more consistent way to determine what size we should draw the image.
-        Bitmap bitmap = decodeSampledBitmapFromResource(getResources(), R.drawable.game_board_nocities, 800, 400);
+        Bitmap bitmap = boardDrawer.createBitmap(getResources(), R.drawable.game_board_nocities, 800, 400);
 
-        // Use a canvas that makes drawing easy
-        Canvas canvas = new Canvas(bitmap);
-        canvas.drawBitmap(bitmap, 0, 0, null);
-
-        final int width = canvas.getWidth();
-        final int height = canvas.getHeight();
-
-        // Not sure how the pixel locations are related to the initial images. These are simply
-        // arbitrary proof-of-concept lines to demonstrate that this works.
-        Paint p = new Paint();
-        p.setColor(Color.WHITE);
-        canvas.drawLine(80, 80, 350, 350, p);
-        canvas.drawLine(80, 80, 1900, 1000, p);
-        canvas.drawLine(80, 80, 1900, 350, p);
-        canvas.drawLine(80, 80, 350, 1000, p);
+        // Draw everything onto the bitmap
+        boardDrawer.drawBoard(bitmap);
 
         // Display the game board
         boardImageView = (ImageView) view.findViewById(R.id.board_image_view);
@@ -111,62 +99,6 @@ public class BoardFragment extends Fragment implements View.OnTouchListener {
         displayPlayerRolesDialog();
 
         return view;
-    }
-
-    /**
-     * Construct a Bitmap of the given resource that is as small as possible while remaining
-     * larger than the given width and height.
-     *
-     * @param res The resources from which the image is located.
-     * @param resId The specific id of the image to generate a Bitmap for.
-     * @param reqWidth The width constraint.
-     * @param reqHeight The height constraint.
-     * @return A Bitmap such that the width and height are as small as possible while remaining
-     * larger than the reqWidth and reqHeight.
-     */
-    private Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
-                                                         int reqWidth, int reqHeight) {
-
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(res, resId, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        options.inMutable = true;
-        return BitmapFactory.decodeResource(res, resId, options);
-    }
-
-    /**
-     * Determine the extent to which the Bitmap dimensions should be reduced from the original image
-     * size using the given constraints. The reduction is determined such that the original image is
-     * reduced to be as small as possible while remaining larger than the given width and height.
-     *
-     * @param options The Bitmap options from which the original image dimensions are retrieved.
-     * @param reqWidth The width constraint.
-     * @param reqHeight The height constraint.
-     *
-     * @return The sample size by which the original image should be reduced.
-     */
-    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw width and height of the image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-        // width and height larger than the requested width and height.
-        while ((height / inSampleSize) > reqHeight
-                && (width / inSampleSize) > reqWidth) {
-            inSampleSize *= 2;
-        }
-        inSampleSize /= 2;
-
-        return inSampleSize;
     }
 
     @Override
