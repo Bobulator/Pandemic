7e795e10768f22557c44f1589dc8f5420922ab0d
Chad Bacon <Chadsbacon@gmail.com>
2016-03-26 00:32:14 -0600

    Changed the project maker to Gradle. Changed the project structure to be Gradle compliant. Fixed a bug that resulted in an Out Of Memory Error.

--- src/com/cs428/pandemic/frontEnd/enums/Role.java
+++
@@ -1,9 +0,0 @@
-package com.cs428.pandemic.frontEnd.enums;
-
-/**
- * Created by Chad Bacon on 2/16/2016.
- * Contains each role that can be assigned to a player.
- */
-public enum Role {
-    MEDIC, RESEARCHER, SCIENTIST, DISPATCHER, OPERATIONS_EXPERT, CONTAINMENT_SPECIALIST
-}
