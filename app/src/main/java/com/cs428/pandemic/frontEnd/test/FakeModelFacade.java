d14828883044dca6307f08d52b5c154fd3835bd5
Chad <chadsbacon@gmail.com>
2016-03-31 01:14:50 -0600

    Implemented the getCityData method in the FakeModelFacade so that it returns a list of cities.

--- app/src/main/java/com/cs428/pandemic/frontEnd/test/FakeModelFacade.java
+++ app/src/main/java/com/cs428/pandemic/frontEnd/test/FakeModelFacade.java
@@ -14,6 +14,7 @@ import com.cs428.pandemic.frontEnd.enums.DiseaseColor;
 import com.cs428.pandemic.frontEnd.enums.Role;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -118,8 +119,61 @@ public class FakeModelFacade implements IModelInterface {
 
     @Override
     public Map<String, UI_City> getCityData() {
-        // TODO: 2/27/2016 (Chad) populate this method
-        return null;
+        HashMap<String, UI_City> cities = new HashMap<>();
+        // Blue
+        cities.put("SanFrancisco", new UI_City("SanFrancisco", DiseaseColor.BLUE, null, Arrays.asList("Tokyo", "Manila", "Chicago", "LosAngeles")));
+        cities.put("Chicago", new UI_City("Chicago", DiseaseColor.BLUE, null, Arrays.asList("SanFrancisco", "LosAngeles", "MexicoCity", "Atlanta", "Montreal")));
+        cities.put("Atlanta", new UI_City("Atlanta", DiseaseColor.BLUE, null, Arrays.asList("Chicago", "Miami", "Washington")));
+        cities.put("Montreal", new UI_City("Montreal", DiseaseColor.BLUE, null, Arrays.asList("Chicago", "Washington", "NewYork")));
+        cities.put("Washington", new UI_City("Washington", DiseaseColor.BLUE, null, Arrays.asList("Atlanta", "Miami", "NewYork", "Montreal")));
+        cities.put("NewYork", new UI_City("NewYork", DiseaseColor.BLUE, null, Arrays.asList("Montreal", "Washington", "Madrid", "London")));
+        cities.put("Madrid", new UI_City("Madrid", DiseaseColor.BLUE, null, Arrays.asList("NewYork", "SaoPaulo", "Algiers", "Paris", "London")));
+        cities.put("London", new UI_City("London", DiseaseColor.BLUE, null, Arrays.asList("NewYork", "Madrid", "Paris", "Essen")));
+        cities.put("Paris", new UI_City("Paris", DiseaseColor.BLUE, null, Arrays.asList("Madrid", "Algiers", "Milan", "Essen", "London")));
+        cities.put("Essen", new UI_City("Essen", DiseaseColor.BLUE, null, Arrays.asList("London", "Paris", "Milan", "StPetersburg")));
+        cities.put("Milan", new UI_City("Milan", DiseaseColor.BLUE, null, Arrays.asList("Paris", "Istanbul", "Essen")));
+        cities.put("StPetersburg", new UI_City("StPetersburg", DiseaseColor.BLUE, null, Arrays.asList("Essen", "Istanbul", "Moscow")));
+        // Yellow
+        cities.put("LosAngeles", new UI_City("LosAngeles", DiseaseColor.YELLOW, null, Arrays.asList("Sydney", "MexicoCity", "Chicago", "SanFrancisco")));
+        cities.put("MexicoCity", new UI_City("MexicoCity", DiseaseColor.YELLOW, null, Arrays.asList("LosAngeles", "Lima", "Bogota", "Miami", "Chicago")));
+        cities.put("Lima", new UI_City("Lima", DiseaseColor.YELLOW, null, Arrays.asList("MexicoCity", "Santiago", "Bogota")));
+        cities.put("Santiago", new UI_City("Santiago", DiseaseColor.YELLOW, null, Arrays.asList("Lima")));
+        cities.put("Bogota", new UI_City("Bogota", DiseaseColor.YELLOW, null, Arrays.asList("MexicoCity", "Lima", "BuenosAires", "SaoPaulo", "Miami")));
+        cities.put("Miami", new UI_City("Miami", DiseaseColor.YELLOW, null, Arrays.asList("Atlanta", "MexicoCity", "Bogota", "Washington")));
+        cities.put("BuenosAires", new UI_City("BuenosAires", DiseaseColor.YELLOW, null, Arrays.asList("Bogota", "SaoPaulo")));
+        cities.put("SaoPaulo", new UI_City("SaoPaulo", DiseaseColor.YELLOW, null, Arrays.asList("Bogota", "BuenosAires", "Lagos", "Madrid")));
+        cities.put("Lagos", new UI_City("Lagos", DiseaseColor.YELLOW, null, Arrays.asList("SaoPaulo", "Kinshasha", "Khartoum")));
+        cities.put("Kinshasha", new UI_City("Kinshasha", DiseaseColor.YELLOW, null, Arrays.asList("Lagos", "Johannesburg", "Khartoum")));
+        cities.put("Johannesburg", new UI_City("Johannesburg", DiseaseColor.YELLOW, null, Arrays.asList("Kinshasha", "Khartoum")));
+        cities.put("Khartoum", new UI_City("Khartoum", DiseaseColor.YELLOW, null, Arrays.asList("Lagos", "Kinshasha", "Johannesburg", "Cairo")));
+        // Black
+        cities.put("Algiers", new UI_City("Algiers", DiseaseColor.BLACK, null, Arrays.asList("Madrid", "Cairo", "Istanbul", "Paris")));
+        cities.put("Cairo", new UI_City("Cairo", DiseaseColor.BLACK, null, Arrays.asList("Algiers", "Khartoum", "Baghdad", "Istanbul")));
+        cities.put("Istanbul", new UI_City("Istanbul", DiseaseColor.BLACK, null, Arrays.asList("Milan", "Algiers", "Cairo", "Baghdad", "Moscow", "StPetersburg")));
+        cities.put("Baghdad", new UI_City("Baghdad", DiseaseColor.BLACK, null, Arrays.asList("Istanbul", "Cairo", "Riyadh", "Karachi", "Tehran")));
+        cities.put("Moscow", new UI_City("Moscow", DiseaseColor.BLACK, null, Arrays.asList("StPetersburg", "Istanbul", "Tehran")));
+        cities.put("Riyadh", new UI_City("Riyadh", DiseaseColor.BLACK, null, Arrays.asList("Cairo", "Karachi", "Baghdad")));
+        cities.put("Tehran", new UI_City("Tehran", DiseaseColor.BLACK, null, Arrays.asList("Moscow", "Baghdad", "Karachi", "Delhi")));
+        cities.put("Karachi", new UI_City("Karachi", DiseaseColor.BLACK, null, Arrays.asList("Tehran", "Baghdad", "Riyadh", "Mumbai", "Delhi")));
+        cities.put("Mumbai", new UI_City("Mumbai", DiseaseColor.BLACK, null, Arrays.asList("Karachi", "Chennai", "Delhi")));
+        cities.put("Delhi", new UI_City("Delhi", DiseaseColor.BLACK, null, Arrays.asList("Tehran", "Karachi", "Mumbai", "Chennai", "Kolkata")));
+        cities.put("Chennai", new UI_City("Chennai", DiseaseColor.BLACK, null, Arrays.asList("Delhi", "Mumbai", "Jakarta", "Bangkok", "Kolkata")));
+        cities.put("Kolkata", new UI_City("Kolkata", DiseaseColor.BLACK, null, Arrays.asList("Delhi", "Chennai", "Bangkok", "HongKong")));
+        // Red
+        cities.put("Bangkok", new UI_City("Bangkok", DiseaseColor.RED, null, Arrays.asList("Kolkata", "Chennai", "Jakarta", "HoChiMinhCity", "HongKong")));
+        cities.put("Jakarta", new UI_City("Jakarta", DiseaseColor.RED, null, Arrays.asList("Chennai", "Sydney", "HoChiMinhCity", "Bangkok")));
+        cities.put("Beijing", new UI_City("Beijing", DiseaseColor.RED, null, Arrays.asList("Shanghai", "Seoul")));
+        cities.put("Shanghai", new UI_City("Shanghai", DiseaseColor.RED, null, Arrays.asList("HongKong", "Taipei", "Tokyo", "Seoul", "Beijing")));
+        cities.put("HongKong", new UI_City("HongKong", DiseaseColor.RED, null, Arrays.asList("Kolkata", "Bangkok", "HoChiMinhCity", "Manila", "Taipei", "Shanghai")));
+        cities.put("HoChiMinhCity", new UI_City("HoChiMinhCity", DiseaseColor.RED, null, Arrays.asList("Bangkok", "Jakarta", "Manila", "Taipei", "Shanghai")));
+        cities.put("Seoul", new UI_City("Seoul", DiseaseColor.RED, null, Arrays.asList("Beijing", "Shanghai", "Tokyo")));
+        cities.put("Taipei", new UI_City("Taipei", DiseaseColor.RED, null, Arrays.asList("Shanghai", "HongKong", "Manila", "Osaka")));
+        cities.put("Manila", new UI_City("Manila", DiseaseColor.RED, null, Arrays.asList("Taipei", "HongKong", "HoChiMinhCity", "Sydney", "SanFrancisco")));
+        cities.put("Sydney", new UI_City("Sydney", DiseaseColor.RED, null, Arrays.asList("Manila", "Jakarta", "LosAngeles")));
+        cities.put("Tokyo", new UI_City("Tokyo", DiseaseColor.RED, null, Arrays.asList("Seoul", "Shanghai", "Osaka", "SanFrancisco")));
+        cities.put("Osaka", new UI_City("Osaka", DiseaseColor.RED, null, Arrays.asList("Tokyo", "Taipei")));
+
+        return cities;
     }
 
     @Override
